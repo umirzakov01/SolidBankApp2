c60a80c26d1f59b74951ba2d31eba2de30971ef4
umirzakov01 <yerzhan.umirzakov@nu.edu.kz>
2024-01-11 04:33:58 +0600

    first commit

---
+++ src/main/java/com/example/solidbank/SolidBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.solidbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidBankApplication.class, args);
+	}
+
+}
